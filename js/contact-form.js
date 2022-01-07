d9ad293a0641fd887e0437f45ff341e1a5ee3bcf
Ethan <findwaldoFTLOG@gmail.com>
2022-01-07 12:10:21 -0500

    Add async form handling

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,3 +1,30 @@
 window.addEventListener('load', () => {
     console.log('Loaded contact-form.js!');
+    document.getElementById('contact-form').addEventListener('submit', submitForm);
 })
+
+const submitForm = (e) => {
+    e.preventDefault();
+
+    let form = document.getElementById('contact-form');
+    let formData = new FormData(form);
+
+    if (window.fetch) {
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams(formData).toString()
+        }).then((success) => {
+            if (success.status == 200) {
+                window.NotificationAPI.notify("Success! Your message has been sent.", true);
+            } else {
+                throw new Error('Request failed');
+            }
+        }).catch(() => {
+            window.NotificationAPI.notify("Unexpected error! Please contact me directly using the links below.", false);
+        })
+    } else {
+        // TODO: XMLHttpRequest
+        window.NotificationAPI.notify('Your browser does not support this feature. Please contact me directly using the link below.', false)
+    }
+}
--- js/notification.js
+++ js/notification.js
@@ -1,9 +1,16 @@
 window.addEventListener('load', () => {
-    const NotificationAPI = {
-        notify: (text) => {
+    window.NotificationAPI = {
+        notify: (text, successful) => {
+            let bannerElement = document.getElementById('notification');
             let textElement = document.getElementById('notification-text');
-            if (textElement) {
-                textElement.innerHTML = text;
+
+            if (bannerElement) {
+                bannerElement.style.display = "block";
+                bannerElement.style.backgroundColor = successful ? "#92B5A6" : "#E09B5E";  // Colors from global.scss.
+
+                if (textElement) {
+                    textElement.innerHTML = text;
+                }
             }
         }
     }
